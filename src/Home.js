be3ee5abfb6a2b9c9c43d54b58a78f932e164e3c
nilesh6423 <nilesh6423@gmail.com>
2022-06-12 16:42:37 +0530

    carousel and hover

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,58 @@
+import { AlternateEmail } from '@mui/icons-material';
 import React from 'react'
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css';
 import "./Home.css"
 import Product from './Product'
 function Home() {
+
+    const properties = {
+        buttonsDisabled: true,
+        dotsDisabled: false,
+        autoPlay: true,
+        autoHeight: true,
+        duration: 1000,
+        mouseTrackingEnabled: false,
+        touchTrackingEnabled: false,
+        swipeDisabled: true,
+        preventEventOnTouchMove: true,
+        autoPlayInterval: 2000,
+        disableAutoPlayOnAction: false,
+        infinite: true,
+    };
+
+    const slideImages = [
+        `${process.env.PUBLIC_URL}/slide_1.jpg`,
+        `${process.env.PUBLIC_URL}/slide_2.jpg`,
+        `${process.env.PUBLIC_URL}/slide_3.jpg`,
+        `${process.env.PUBLIC_URL}/slide_4.jpg`,
+        `${process.env.PUBLIC_URL}/slide_5.jpg`,
+    ];
+
     return (
         <div className='home'>
             <div className='home__container'>
-                <img
-                    className='home__image'
-                    src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-                    alt=''
-                />
+                <AliceCarousel {...properties}>
+                    {slideImages.map((each, index) => (
+                        <img
+                            className='home__image'
+                            src={each}
+                            alt="sample"
+                        />
+                    ))}
+                </AliceCarousel>
                 <div className='home__row'>
                     <Product
                         id="146672"
                         title="The Secret Hardcover – 1 January 2009 by Rhonda Byrne"
-                        price={468}
+                        price={468.00}
                         image="https://m.media-amazon.com/images/I/91Wj9JEjtiL._AC_UY327_FMwebp_QL65_.jpg"
                         rating={5}
                     />
                     <Product
                         id="134164"
                         title="Mamaearth Ubtan Natural Face Wash for All Skin Type with Turmeric & Saffron for Tan removal and Skin brightning 100 ml - SLS & Paraben Free"
-                        price={228}
+                        price={228.00}
                         image="https://m.media-amazon.com/images/I/51hwUO-kAqL._SL1200_.jpg"
                         rating={3}
                     />
@@ -38,14 +69,14 @@ function Home() {
                     <Product
                         id="550570"
                         title="Xiaomi 11 Lite NE 5G (Vinyl Black 6GB RAM 128 GB Storage) | Slimmest (6.81mm) & Lightest (158g) 5G Smartphone | 10-bit AMOLED with Dolby Vision |"
-                        price={24999}
+                        price={24999.00}
                         image="https://m.media-amazon.com/images/I/71xZXmNJ6UL._SL1500_.jpg"
                         rating={4}
                     />
                     <Product
                         id="679735"
                         title="Armani Exchange Banks Analog Black Dial Men's Watch-AX7133SET"
-                        price={15995}
+                        price={15995.00}
                         image="https://m.media-amazon.com/images/I/61otyf3psdL._UL1500_.jpg"
                         rating={5}
                     />
@@ -55,7 +86,7 @@ function Home() {
                     <Product
                         id="822921"
                         title="Mi 125.7 cm (50 Inches) 4K Ultra HD Android Smart LED TV 4X | L50M5-5AIN (Black)"
-                        price={25999}
+                        price={25999.00}
                         image="https://m.media-amazon.com/images/I/61V7cDH8AAS._SL1188_.jpg"
                         rating={4}
                     />
